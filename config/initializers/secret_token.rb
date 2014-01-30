9d9c7f7ee7f526833f68fa7c1ef41416c8ba80eb
Michael Schmidt <d038720@mo-78bf75665.mo.sap.corp>
2014-01-30 13:09:47 +0000

    inital

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hana::Application.config.secret_key_base = '762b1fe216a4c18302676ac7abb811effbc96c30816fbb64bdce890a2721a04700967ada640d5f4e132b7e4689ae72f78892239c0a9a94956634e1bb167b8304'
